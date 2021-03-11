fcb7033bbe16dcb5d3b0aea37854c19740f1e16d
‘Juliano <julianoperins@gmail.com>
2021-03-11 16:56:15 -0500

    Created the home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
-// import Home from "./components/pages/Home";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 }
